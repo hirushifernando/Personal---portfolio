6887c986e98ec82d67b124cb1b14337b75816e89
Hirushi <hirufernando1107@gmail.com>
2024-03-31 23:28:34 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer", "UI/UX Designer", "Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
