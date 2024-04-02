fe51f216937ac179a964cdf6a7e951739cea3c3a
Hirushi <hirufernando1107@gmail.com>
2024-04-02 11:06:43 +0530

    New

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Full stack Developer","Frontend Web Developer", "UI/UX Designer"],
+    strings: ["Full stack Developer","Front-end Web Designer", "UI/UX Designer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
